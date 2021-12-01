6833bd2652d4a02791b83459b69d9d243998074b
Mladjo Milunovic <mladjo.milunovic@doob3dlabs.com>
2021-12-01 21:28:20 +0100

    changed triplets from lists to tuples in day 01

--- day01/main.py
+++ day01/main.py
@@ -8,6 +8,6 @@ measurements = [int(i) for i in text.splitlines()]
 increases = [measurements[i] for i in range(1, len(measurements)) if measurements[i] > measurements[i-1]]
 print(len(increases))
 
-triplets = [[measurements[i-1], measurements[i], measurements[i+1]] for i in range(1, len(measurements) - 1)]
+triplets = [(measurements[i-1], measurements[i], measurements[i+1]) for i in range(1, len(measurements) - 1)]
 increases_triplets = [triplets[i] for i in range(1, len(triplets)) if sum(triplets[i]) > sum(triplets[i-1])]
 print(len(increases_triplets))
