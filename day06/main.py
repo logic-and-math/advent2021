ec1ea95e8ccf877cbb8abfa6efb98935e082af51
Mladjo Milunovic <mladjo.milunovic@doob3dlabs.com>
2021-12-06 17:18:31 +0100

    second part

--- day06/main.py
+++ day06/main.py
@@ -12,5 +12,21 @@ def calc(start_day, counter, n_days) -> int:
     new_fishes = [f for f in new_fishes if f[0] <= n_days] # real
     return len(new_fishes) + sum([calc(fish[0], fish[1], fish[2]) for fish in new_fishes])
 
-
 print(sum(calc(0, n, 80) for n in numbers) + len(numbers))
+
+values = [0] * 9
+for n in numbers:
+    values[n] += 1
+
+for day in range(256):
+    n_fishes_to_reset = values[0]
+    n_new_fishes = values[0]
+
+    new_values = [values[i+1] for i in range(0, len(values) - 1)] + [0]
+    new_values[6] += n_fishes_to_reset
+    new_values[8] = n_new_fishes
+    
+    values = new_values
+
+print(sum(values))
+
